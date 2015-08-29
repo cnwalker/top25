c11060c82422b45329b90f691816b6eec8c54716
Christopher Walker <cnwalker@uchicago.edu>
2015-08-29 17:18:17 -0400

    Fixed issue where words of length three would not be recognized as stems, deleted .pyc files, and other cleaning up

--- wsgi/openshift/readers/WordAnalyzer/StemFinder.py
+++ wsgi/openshift/readers/WordAnalyzer/StemFinder.py
@@ -9,7 +9,7 @@ def word_ref(base, editted, ref):
         return base
 
 def getStem(word, all_words, irregular):
-    if len(word) < 4 and word not in all_words:
+    if len(word) < 4 or word not in all_words:
     #Words of length three or less are stems
         return word
     else:
@@ -22,11 +22,11 @@ def getStem(word, all_words, irregular):
         word = word[:-2] + word[-1]
     if word[-2:] == "s'":
         word = word[:-1]
-    
+
     #Creates the checkStem function to evaluate whether a stemmed
     #word is a real word. If it isn't, returns the original word
     checkStem = lambda x: word_ref(word, x, all_words)
-    
+
     if word[-1] == 's':
         if word[-4:] == 'ness' and len(word) > 4:
             #Requiring word greater than 4 because Ness is also a proper noun
@@ -39,7 +39,7 @@ def getStem(word, all_words, irregular):
             if word[-3] == 'i' and len(word) > 4:
             #words ending in ies -> y example babies -> baby
                 return checkStem(word[:-3] + 'y')
-            #Words ending in 'es' are plural if preceeded by ch, s or x 
+            #Words ending in 'es' are plural if preceeded by ch, s or x
             if word[-3] in ['s', 'x'] or word[-4:][:-2] == 'ch':
                 if word[-3] == 'v':
                     word[-3] = 'f'
@@ -53,7 +53,7 @@ def getStem(word, all_words, irregular):
 
     if word[-2:] == 'ae':
         return checkStem(word[:-1])
-    
+
     if word[-2:] == 'ly':
     #Example: quickly -> quick
         if word[:-2][-3:] == 'ing' and len(word) > 6:
@@ -64,7 +64,7 @@ def getStem(word, all_words, irregular):
 
     if word[-2:] == 'ed':
     #Removes ed from back of word, expect in case of stem change
-    #Example: talked -> talk, stopped -> stop, freed -> free 
+    #Example: talked -> talk, stopped -> stop, freed -> free
         if word[-3] in ['e', 'o', 'y'] and word[:-1] in all_words:
             return checkStem(word[:-1])
         if word[-3] == word[-4]:
@@ -75,7 +75,7 @@ def getStem(word, all_words, irregular):
         if word[:-1] in all_words:
             return checkStem(word[:-1])
         return checkStem(word[:-2])
-      
+
     if word[-3:] == 'ing' and len(word) > 4:
     #Removes 'ing' from back of word, one extra in case of stem change
     #Example: bedding -> bed, frying -> fry
